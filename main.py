6c1bab79f6970df2067811047e3ddaffd7b6ee9b
PierreNabil <pipopierrenabil@gmail.com>
2020-12-09 11:15:31 +0200

    hello

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from protocol import FrameKind
 MAX_TIME = 100
 
 # [(t1, packet1), (t2, packet2), ...]
-network_layer_for_computer1 = [(0, c) for c in 'Hello World!'] + [(30, c) for c in 'HAHA']
+network_layer_for_computer1 = [(0, c) for c in 'Hello World!'] #+ [(30, c) for c in 'HAHA']
 network_layer_for_computer2 = [(0, c) for c in 'Welcome.']
 
 # {t_i, t_j, t_k, ...}
@@ -22,13 +22,13 @@ for t in range(MAX_TIME):
 	events = computer1.check_events(wire_from_2_to_1, t)
 	wire_from_1_to_2 = computer1.make_decision(events, wire_from_2_to_1, t)
 
-	if t in error_times_for_computer_1:
+	if t is not None and t in error_times_for_computer_1:
 		wire_from_1_to_2.kind = FrameKind.err
 
 	events = computer2.check_events(wire_from_1_to_2, t)
 	wire_from_2_to_1 = computer2.make_decision(events, wire_from_1_to_2, t)
 
-	if t in error_times_for_computer_2:
+	if t is not None and t in error_times_for_computer_2:
 		wire_from_2_to_1.kind = FrameKind.err
 
 print(computer1.network_layer_data_received)
